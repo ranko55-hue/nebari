1c4552032b2414d44e98821879932fec4339801a
ranko55-hue <ranko55@gmail.com>
2026-07-19 17:09:53 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import './styles/theme.css'
 import './lib/i18n' // sets lang/dir before first paint
 
 ReactDOM.createRoot(document.getElementById('root')).render(
